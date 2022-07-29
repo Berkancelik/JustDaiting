a95c80205e08b26a59c2822de52f76df9ff1cbc2
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-29 18:24:37 +0300

    Db Context created

---
+++ DataAccessLayer/Concrete/JustDaitingContext.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Concrete
+{
+    public class JustDaitingContext :IdentityDbContext<AppRole,AppUser,string>
+    {
+        public JustDaitingContext(DbContextOptions<JustDaitingContext> options) : base(options)
+        {
+
+        }
+    }
+}

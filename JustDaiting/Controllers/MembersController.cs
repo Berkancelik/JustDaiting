bf8de3c519640e0ce2f6649b38b9ea98ebb1f9f3
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-02 11:46:59 +0300

    Designing the member homepage

--- JustDaiting/Controllers/MembersController.cs
+++ JustDaiting/Controllers/MembersController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using EntityLayer.Concrete;
+using JustDaiting.ViewModels;
+using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JustDaiting.Controllers
@@ -6,9 +10,22 @@ namespace JustDaiting.Controllers
     [Authorize]
     public class MembersController : Controller
     {
+        public UserManager<AppUser> userManager { get; }
+        public SignInManager<AppUser> signInManager { get; }
+
+        public MembersController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            AppUser user = userManager.FindByNameAsync(User.Identity.Name).Result;
+            UserViewModel userViewModel = user.Adapt<UserViewModel>();
+
+            return View(userViewModel);
         }
     }
-}
+
+} 
